More examples + cleaning + correct scaling in objective functional

ADMM code/example_plane_two_disks.py
@@ -1,5 +1,5 @@
 """ 
-Produce the interpolation which corresponds to Figure 2 of the article 
+Produce the interpolation which corresponds to Figure 3 of the article 
 """
 
 import os
@@ -39,7 +39,7 @@ engine.start()
 nTime = 31
 
 # Name of the file in which is stored the triangulated surface D 
-nameFileD = os.path.join("meshes", "face_vector_field_319.off")
+nameFileD = os.path.join("meshes", "square_regular_100.off")
 
 # Parameter epsilon to regularize the Laplace problem 
 eps = 0.0*10**(-8)
@@ -51,7 +51,7 @@ Nit = 1000
 detailStudy = False 
 
 # Value for the congestion parameter (alpha in the article)
-cCongestion = 0.1 
+cCongestion = 0.0
 
 # -----------------------------------------------------------------------------------------------
 # Read the .off file  
@@ -75,30 +75,23 @@ nVertices = Vertices.shape[0]
 mub0 = np.zeros(nVertices)
 mub1 = np.zeros(nVertices)
 
-lengthScale = 0.1 
-
-# Center of the "blobs" for mub0 and mub1
-center0 = Vertices[4492,:]
-center1 = Vertices[4225,:]
+# Center of the "blob" for mub0 
+x0 = np.array([0.33,0.5,0.0])
+# Center of the "blob" for mub1 
+x10 = np.array([0.8,0.2,0.0])
+x11 = np.array([0.8,0.8,0.0])
 
 for i in range(nVertices) :
-	
-	# Change of coordinates 
-	alpha = 0.1 * Vertices[i,0] + Vertices[i,1] 
-	beta = - Vertices[i,0] + 0.1 * Vertices[i,1] 
-	gamma = Vertices[i,2]
 
-	# Define mub0
-	if gamma >= -0.1 :		
-		mub0[i] = areaVertices[i] * cut_off.f(-0.2-alpha,0.3) * cut_off.f(alpha - 0.15,0.3) * cut_off.f(0.1 - beta,0.3) * cut_off.f(beta - 0.45,0.3)
-		
-	# Define mub1 
-	mub1[i] += areaVertices[i] * exp( -lin.norm( Vertices[i,:] - center0  )**2 / lengthScale**2  )
-	mub1[i] += areaVertices[i] * exp( -lin.norm( Vertices[i,:] - center1  )**2 / lengthScale**2  )
-		
+	mub0[i] = areaVertices[i] * cut_off.f(lin.norm(Vertices[i,:] - x0) - 0.1 ,0.1)
+	mub1[i] += areaVertices[i] * cut_off.f((lin.norm(Vertices[i,:] - x10) - 0.1)*2. ,0.1)
+	mub1[i] += areaVertices[i] * cut_off.f((lin.norm(Vertices[i,:] - x11) - 0.1)*2. ,0.1)
+	
 # Normalization 
 mub0 /= np.sum(mub0)
 mub1 /= np.sum(mub1)
+
+	
 	
 # -----------------------------------------------------------------------------------------------
 # Call the algorithm   
@@ -120,7 +113,7 @@ plt.show()
 # Saving the results 
 # -----------------------------------------------------------------------------------------------
  
-np.savetxt("face_mu_store.txt", mu.reshape((nTime*nVertices)) )
+np.savetxt("plane_two_disks_mu_store.txt", mu.reshape((nTime*nVertices)) )
 
 # -----------------------------------------------------------------------------------------------
 # Printing the results with mayavi 
@@ -131,14 +124,8 @@ np.savetxt("face_mu_store.txt", mu.reshape((nTime*nVertices)) )
 def parametersMayavi(scene) : 
 
 	# Put the right angle 
-	scene.scene.camera.position = [-0.069944662502985533, 1.2526435392885236, 3.9345675528530402]
-	scene.scene.camera.focal_point = [0.0052490000000000037, 0.06885150000000001, 0.033782499999999993]
-	scene.scene.camera.view_angle = 30.0
-	scene.scene.camera.view_up = [0.99362963464680065, -0.10108021570105792, 0.049829099384727987]
-	scene.scene.camera.clipping_range = [2.5610091024871435, 6.0229878620594661]
-	scene.scene.camera.compute_view_plane_normal()
-	scene.scene.render()
-	
+	scene.scene.z_minus_view()
+
 	# Background color 
 	scene.scene.background = (1.0, 1.0, 1.0)
 
@@ -155,7 +142,7 @@ for i in [0,(nTime-1) // 4,  (nTime-1) // 2, (3*nTime-1) // 4, nTime-1] :
 	parametersMayavi(engine.scenes[0])
 
 	# Uncomment to save the figure 
-	# mlab.savefig("face_mu_" + str(i)+ ".png", size = (300,500))	
+	# mlab.savefig("plane_two_disks_mu_" + str(i)+ ".png", size = (500,500))	
 	# mlab.close()
 
 	mlab.show()

ADMM code/geodesic_surface_congested.py
@@ -179,7 +179,7 @@ def geodesic(nTime, nameFileD, mub0,mub1, cCongestion, eps, Nit, detailStudy) :
 			objectiveValue[3*counterMain] = objectiveFunctional(phi,mu,A,E,B,lambdaC,BT,geomDic,r,cCongestion,isCongestion)
 		elif (counterMain % 10) == 0 : 
 			objectiveValue[ counterMain // 10 ] = objectiveFunctional(phi,mu,A,E,B,lambdaC,BT,geomDic,r,cCongestion,isCongestion)
-		
+			
 		# Laplace problem -----------------------------------------------------------------------------
 
 		startLaplace = time.time()
@@ -194,17 +194,15 @@ def geodesic(nTime, nameFileD, mub0,mub1, cCongestion, eps, Nit, detailStudy) :
 		RHS += np.multiply( r*gradATime(  A + lambdaC, geomDic  ), areaVerticesGlobalStaggerred /3)  
 		
 		# We take the adjoint wrt tp the scalar product weighted by areas, hence the multiplication by areaVectorized
-		# RHS -=  divergenceD( np.multiply(E,areaVectorized)   )
 		RHS -=  divergenceD( E, geomDic  )	
 		RHS +=  r*divergenceD( np.multiply(B,areaVectorized), geomDic  ) 	
 		
 		# Solve the system 
-		
 		phi = 1./r * LaplacianInvert(  RHS )
 		
 		endLaplace = time.time()
 		print( "Solving the Laplace system: " + str( round( endLaplace - startLaplace, 2) ) + "s." )
-		
+				
 		if detailStudy:
 			objectiveValue[3*counterMain + 1] = objectiveFunctional(phi,mu,A,E,B,lambdaC,BT,geomDic,r,cCongestion,isCongestion)
 		
@@ -281,7 +279,7 @@ def geodesic(nTime, nameFileD, mub0,mub1, cCongestion, eps, Nit, detailStudy) :
 		if detailStudy:
 			objectiveValue[3*counterMain + 2] = objectiveFunctional(phi,mu,A,E,B,lambdaC,BT,geomDic,r,cCongestion,isCongestion)	
 		
-		# Gradient descent in (E,muTilde), i.e. in the dual ----------------------------------------------- 
+		# Gradient descent in (E,muTilde), i.e. in the dual --------------------------------------
 			
 		# No need to recompute the derivatives of phi  
 		
@@ -388,10 +386,8 @@ def scalarProductFun(a,b,geomDic) :
 def scalarProductTriangles(a,b,geomDic) :
 	"""
 	Scalar product weighted by the area of the Triangles 
-	"""
-	aux1 = np.multiply(a,b)
-	aux2 = np.multiply( aux1, geomDic['areaVectorized'] )
-	return np.sum( aux2) / geomDic['nTime'] 	
+	"""	
+	return np.sum( np.multiply( np.multiply(a,b), geomDic['areaVectorized'] ) ) / geomDic['nTime'] 	
 	
 
 #********************************************************************************************************
@@ -468,7 +464,7 @@ def divergenceD(input,geomDic) :
 	# Transpose and reshape to apply divergenceDMatrix independently for fixed first two indices 
 	outputAux = geomDic['divergenceDMatrix'].dot(inputAux.transpose()).transpose().reshape((geomDic['nTime'],2,3,geomDic['nVertices']))
 	
-	# Then sim in both time and vertex 
+	# Then sum in both time and vertex 
 	output[:-1,:] += (outputAux[:,0,0,:] + outputAux[:,0,1,:] + outputAux[:,0,2,:])
 	output[1:,:] += (outputAux[:,1,0,:] + outputAux[:,1,1,:] + outputAux[:,1,2,:])	
 	
@@ -487,7 +483,7 @@ def objectiveFunctional(phi,mu,A,E,B,lambdaC,BT,geomDic,r,cCongestion,isCongesti
 	# Boundary term 
 	output += np.dot( phi[0,:], BT[0,:] ) + np.dot( phi[-1,:], BT[-1,:] )
 	
-	# Computing the derivatives of phi and split them in D 
+	# Computing the derivatives of phi
 	dTphi = gradTime(phi,geomDic)
 	dDphi = gradientD(phi,geomDic)
 	
@@ -495,17 +491,17 @@ def objectiveFunctional(phi,mu,A,E,B,lambdaC,BT,geomDic,r,cCongestion,isCongesti
 	output += scalarProductFun( A + lambdaC - dTphi, mu, geomDic )
 	
 	# Lagrange multiplier E. 
-	output += np.sum(  np.multiply(   B - dDphi, E )) 
+	output += np.sum(  np.multiply(   B - dDphi, E )) / geomDic['nTime'] 
 	
 	# Penalization in lambda, only in the case of congestion
 	if isCongestion : 
 		output -= 1/(2.*cCongestion) * scalarProductFun( lambdaC, np.multiply(lambdaC, geomDic['areaVerticesGlobal']/3.),geomDic )
 	
 	# Penalty in A, phi 
-	output -= r/2 * scalarProductFun( A + lambdaC - dTphi, np.multiply(A + lambdaC - dTphi, geomDic['areaVerticesGlobal']/3.),geomDic ) 
+	output -= r/2. * scalarProductFun( A + lambdaC - dTphi, np.multiply(A + lambdaC - dTphi, geomDic['areaVerticesGlobal']/3.),geomDic ) 
 	
 	# Penalty in B, phi. 
-	output -= r/2* scalarProductTriangles( B - dDphi, B - dDphi, geomDic )
+	output -= r/2. * scalarProductTriangles( B - dDphi, B - dDphi, geomDic )
 	
 	return output
 	